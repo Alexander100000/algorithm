0e8f96298cb79069276ca170cb3dc92b16fcc691
xrezat <558081xr>
2022-03-27 18:11:46 +0800

    countDownLatchDemo

---
+++ src/main/java/Main0003.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.CountDownLatch;
+/*
+* CountDownLatch 又是熟悉的生产者和消费者模型给定缓冲区为7然后让生产者生产消息一个一个投递缓存区的容量一个一个的做减法，一直到为zero然后
+* 才会调用countDownLatch的await方法然后通知消费者消费者消费完然后阻塞通知生产者。
+ *
+*
+* */
+public class Main0003 {
+    private static final int STUDENTS =7;
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch=new CountDownLatch(STUDENTS);
+        for(int i=0;i<7;i++){
+            new Thread(()->{System.out.println(Thread.currentThread().getName()+"开完会离开，会议室");
+//            进行--操作
+                countDownLatch.countDown();
+            },String.valueOf(i)).start();
+
+        }
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName()+"会议组织负责人关门，关门");
+    }
+
+}
